25df6de83c85dd485ccbf462f5837fc26d7abad9
Anna Edmund <annecreeyates@gmail.com>
2023-06-13 21:44:34 +0100

    in index.html: added placeholders for sections; in main.js: added request in node.js (as shown in the website documentation)

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 require("dotenv").config(); //dotenv configuratoin checked - works
+
+const apiKey = process.env.API_KEY;
+
+// const pollenPara = document.getElementById("pollenInfo");
+
+const http = require("https");
+
+const options = {
+  method: "GET",
+  hostname: "api.ambeedata.com",
+  port: null,
+  //check for the place for format string. Take from user input.
+  path: "/forecast/pollen/by-place?place=Bengaluru",
+  headers: {
+    "x-api-key": apiKey,
+    "Content-type": "application/json",
+  },
+};
+
+const req = http.request(options, function (res) {
+  const chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
