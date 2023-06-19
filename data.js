86b141ad36dfd19372b8e6d813aad8ee53738171
Anna Edmund <annecreeyates@gmail.com>
2023-06-19 22:53:32 +0100

    added test.js to gitignore; in data.js: removed all the commented out code that was example of use from ambee page and replaced it with a shorter getData function that uses a new Promise, api key is hidden, using import instead of require; in main.js: removed all the elements that were using DOM and were browser based and changed that to a node.js readFile, which reads and processes the html and css on the server side instead; in index.html: removed type='module';

--- data.js
+++ data.js
@@ -1,63 +1,33 @@
-// import "dotenv/config"; //dotenv configuratoin checked - works
-// import fetch from "node-fetch";
-const apiKey =
-  "037c7144f6166d569c05556d4af373dfe290ba5576ac13f526050269289ac24a";
-// process.env.API_KEY;
+//the data retrieving works fine - at the moment the data results are printed to the console
 
-//node.js for requesting data from ambee api
-//this code comes in full from the ambee website api documentation, from the forecast section
-// const http = require("https");
+import "dotenv/config"; //dotenv configuratoin checked - works
+import fetch from "node-fetch";
+
+const apiKey = process.env.API_KEY;
 
 async function getData(place) {
-  // return new Promise((resolve, reject) => {
   const url = `https://api.ambeedata.com/latest/pollen/by-place?place=${encodeURIComponent(
     place
   )}`;
   const options = {
-    // method: "GET",
-    // hostname: "api.ambeedata.com",
-    // port: null,
-    //check for the place for format string. Take from user input.
-
     headers: {
       "x-api-key": apiKey,
       "Content-type": "application/json",
     },
   };
-
-  // const req = http.request(options, function (res) {
-  //   const chunks = [];
-
-  //   res.on("data", function (chunk) {
-  //     chunks.push(chunk);
-  //   });
-
-  //   res.on("end", function () {
-  //     const body = Buffer.concat(chunks);
-  //     resolve(body.toString());
-  //   });
-  // });
-
-  // req.on("error", (err) => {
-  //   reject(err);
-  // });
-
-  // req.end();
-  //end of the api request node.js
-
   //this was a server side request with the use on node.js, it would need a more complicated server setup
-  // fetch(url, options)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     resolve(data);
-  //   })
-  //   .catch((error) => {
-  //     reject(error);
-  //   });
-  // });
-  const response = await fetch(url, options);
-  const data = await response.json();
-
-  return data;
+  const responseHandlingPromise = new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+        console.dir(data, { depth: null });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
+
+// console.log(getData("London"));
 export { getData };
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 import { getData } from "./data.js";
+import * as fs from "fs";
+import * as http from "http";
 
-const location = document.getElementById("userInput_loc");
-// const pollenPara = document.getElementById("pollenInfo");
-console.log(location.value);
-
-location.addEventListener("keydown", async (event) => {
-  if (event.key === "Enter") {
-    const data = await getData(location.value);
-    console.log(data);
-  }
+//read a file (html, css) from the provided relative path and render it in the browser on port 3000
+fs.readFile("./views/index.html", (err, html) => {
+  if (err) {
+    throw err;
+  } //create http server and listen for requests to html and css files
+  http
+    .createServer((req, res) => {
+      res.writeHeader(200, { "Content-type": "text/html" });
+      res.write(html);
+      res.end();
+    })
+    .listen(3000, () => {
+      console.log("Listening for requests on port 3000");
+    });
 });
+
+//listen for the Enter key in the input area in html to receive the location information
+
+//use the location information in the data.js request for retrieving the pollen information
