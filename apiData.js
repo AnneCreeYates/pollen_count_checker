de0062e5b5a4c51ba0a35f98efac11a86bb55ba0
Anna Edmund <annecreeyates@gmail.com>
2023-06-22 00:17:04 +0100

    brought back apiData for making external requests; in app.js added middleware and static and add.post for recieving and using the place input from the user; introduced partial - footer, head and nav; moved styles to public; removed all the sections from 404, index and about files that are imported as partials

---
+++ apiData.js
@@ -0,0 +1,38 @@
+require("dotenv").config(); //dotenv configuratoin checked - works
+//as require is not supported for this module it needs to by dynamic import()
+let fetch;
+import("node-fetch").then(module => {
+  fetch = module.default;
+});
+
+const apiKey = process.env.API_KEY;
+
+//function receives the data from the user sent by the html form input on the browser side
+
+//function sends the request to the website using it's api and key with the purpose of retrieving the pollen data
+async function getData(place) {
+  const url = `https://api.ambeedata.com/latest/pollen/by-place?place=${encodeURIComponent(
+    place
+  )}`;
+  const options = {
+    headers: {
+      "x-api-key": apiKey,
+      "Content-type": "application/json",
+    },
+  };
+  //this was a server side request with the use on node.js, it would need a more complicated server setup
+  const responseHandlingPromise = new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data);
+        // console.dir(data, { depth: null });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  //return promise
+  return responseHandlingPromise;
+}
+module.exports = { getData };
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const apiData = require("./apiData");
 //express app
 const app = express();
 
@@ -9,15 +9,36 @@ app.set("view engine", "ejs");
 //listen for reqiests
 app.listen(3000);
 
+//middleware & static files
+app.use(express.static("public"));
+//takes the encoded data and passess that to the object that can be used on the request object
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.render("index");
+  
+  res.render("index", { title: "Home" });
 });
 
+//is supposed to receive the data from an API using a place parameter
+app.post("/", (req,res) => {
+  const place = req.body.place;
+  console.log(place);
+  apiData.getData(place)
+    .then(data => {
+      res.render("index", { title: "Home", data: data });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send("An error occured");
+    })
+    
+})
+
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
