2477b04a84160cae3db80739e31d8a0bfa8d6f64
Anna Edmund <annecreeyates@gmail.com>
2023-06-21 14:22:26 +0100

    As I decided to work with express.js all the javascript files were moved to kinda working node folder and added to gitignore; created app.js which listens for request on port 3000 and isrendering the index, about and 404 files; the files that were html before changed to ejs for the purpose of view rendering - enables the creation of partials and templating

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+//express app
+const app = express();
+
+//register view engine
+app.set("view engine", "ejs");
+
+//listen for reqiests
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404");
+});
